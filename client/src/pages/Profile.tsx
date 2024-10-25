7974c6d431634b41a778b82a6796415db6c7ea9f
Impra <kiki.impric@gmail.com>
2024-10-25 11:02:45 +0200

    [FE] - Added - added fetch single user

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,12 +1,23 @@
 import { UserContext } from "@context/UserContext";
 import useCallContext from "@hooks/useCallContext";
+import { useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
+import { IUser } from "../../../types/user";
+import { fetchSingleUser } from "@services/User";
 
 const Profile = () => {
     const userContext = useCallContext(UserContext);
     const { id } = useParams();
+    const [foundUser, setFoundUser] = useState<IUser | null>(null);
     
+    const findUser = async (id: number) => {
+        const response = await fetchSingleUser(id);
+        console.log(response);
+        if (!response.error) setFoundUser(response.data.user);
+    }
+
     if (!id) return <Navigate to={'/'} />;
+    if (userContext && userContext.user && userContext.user.id !== +id) findUser(+id);
 
     return (
         <div>
--- client/src/services/User.ts
+++ client/src/services/User.ts
@@ -32,3 +32,19 @@ export const refreshUserAsync = () => {
         })
       .catch((e) => ({ error: true, data: e}));
 }
+
+export const fetchSingleUser = (id: number) => {
+  return fetch(`http://localhost:8000/user/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+  .then(async (response) => { 
+    const data = await response.json();
+    if(!response.ok) return { error: true, data: data};
+    else return { error: false, data: data};
+    })
+  .catch((e) => ({ error: true, data: e}));
+}
