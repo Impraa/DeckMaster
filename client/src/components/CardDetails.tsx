289bde559dcb24eb63d6d26aa6358583917ce1a2
Impra <kiki.impric@gmail.com>
2024-11-13 01:16:38 +0100

    [FE] - Added - monster card details component

--- client/src/components/CardDetails.tsx
+++ client/src/components/CardDetails.tsx
@@ -1,18 +1,22 @@
 import { CardContext } from "@context/CardContext"
 import useCallContext from "@hooks/useCallContext"
 import { Navigate } from "react-router-dom";
+import { isValidMonster } from "../../../types/card";
+import MonsterCardDetails from "./MonsterCardDetails";
 
 const CardDetails = () => {
     const cardContext = useCallContext(CardContext);
 
     if (!cardContext) return <Navigate to={'/'} />
-    
+
     return (
         <div>
-            {cardContext.card ?             
-                <>
-                    <img key={cardContext.card.id} src={`http://localhost:8000${cardContext.card.cardImage}`} />
-                </>
+            {cardContext.card ?
+                isValidMonster(cardContext.card) ?
+                    <MonsterCardDetails monsterCard={cardContext.card}/> :     
+                    <>
+                        <img key={cardContext.card.id} src={`http://localhost:8000${cardContext.card.cardImage}`} />
+                    </>
                 : <p>No card selected</p>
             }
         </div>
