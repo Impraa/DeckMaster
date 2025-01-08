5cca68e1cb11d54a5cc5b8993fbd438687fa71be
Impra <kiki.impric@gmail.com>
2025-01-08 20:32:34 +0100

    [FE] - Added - gatekeep to non admin user to manage cards

--- client/src/pages/ManageCards.tsx
+++ client/src/pages/ManageCards.tsx
@@ -1,6 +1,17 @@
 import CardPool from "@components/CardPool"
+import { UserContext } from "@context/UserContext";
+import useCallContext from "@hooks/useCallContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ManageCards = () => {
+    const userContext = useCallContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!userContext || (userContext && userContext.user && userContext.user.role !== 'ADMIN')) return navigate('/');
+    }, [])
+
     return (
         <div className="w-full">
             <CardPool/>
