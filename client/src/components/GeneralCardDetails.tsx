dbe200d3ef01958fedf36dd32dd11fe5f9b638bb
Impra <kiki.impric@gmail.com>
2025-01-13 12:05:13 +0100

    [FE] - Designed - general card details

--- client/src/components/GeneralCardDetails.tsx
+++ client/src/components/GeneralCardDetails.tsx
@@ -1,11 +1,16 @@
-import { ICard } from "../../../types/card";
+import { ICard, isValidMonster } from "../../../types/card";
 
 const GeneralCardDetails:React.FC<{card: ICard}> = ({ card }) => {
     return (
-        <div>
-            <img key={card.id} src={`http://localhost:8000${card.cardImage}`} className="w-[50%]"/>
-            <h2>{card.name}</h2>
-            <h3>{card.race}</h3>
+        <div className="flex flex-col">
+            <img key={card.id} src={`http://localhost:8000${card.cardImage}`} className="w-[50%] self-center"/>
+            <h2 className="text-xl font-semibold">{card.name}</h2>
+            <h3 className="text-lg">{card.attribute ? card.attribute + ' /' : ''} {card.race}</h3>
+            {isValidMonster(card) && <h3 className="text-lg">ATK/DEF {card.atk}/{card.def}</h3>}
+            <div>
+                <h3 className="text-lg">Effect: </h3>
+                <p className="text-md">{card.cardText}</p>
+            </div>
         </div>
     )
 };
